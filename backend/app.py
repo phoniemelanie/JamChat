471a58543e3ff0e12f29e0eb2c9f0d7ed2176350
Wenzheng Du <34501387+dwz99999@users.noreply.github.com>
2022-08-27 19:00:36 +1000

    add backend for getting users on homepage

--- backend/app.py
+++ backend/app.py
@@ -4,16 +4,33 @@ from bson import ObjectId
 
 app = Flask(__name__)
 
-# API to retrieve all recipes in DB
+@app.route("/")
+def app():
+    input_json = request.get_json(force=True)
+    uid = input_json["user_id"]
+    user = db.user_collection.find_one({"_id": uid})
+    data = []
+    for friend_id in user["friends"]:
+        friend = db.user_collection.find_one({"_id": friend_id})
+        friend["_id"] = str(friend["_id"])
+        # hard-coded embed uri for now
+        friend["spotify_uri"] = "spotify:track:4qDHt2ClApBBzDAvhNGWFd"
+        data.append(friend)
+    return jsonify(data)
+
+# Tomorrow: authentication, spotify tokens, get live tracks from spotify, calculate similarities, deployment
+    
+
+# API to retrieve all users in DB
 @app.route("/getUsers", methods=["POST"])
 def getAll():
-    all_docs = db.user_collection.find()
+    all_users = db.user_collection.find()
     data = []
 
-    for doc in all_docs:
+    for user in all_users:
         # Making it into a string avoids this TypeError: Object of type ObjectId is not JSON serializable
-        doc['_id'] = str(doc['_id']) 
-        data.append(doc)
+        user['_id'] = str(user['_id']) 
+        data.append(user)
 
     return jsonify(data)
 
